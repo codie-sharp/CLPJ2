d63fe552266cf1c03e17112e49afe92019490e11
Codie Sharp <codie.sharp@gmail.com>
2021-10-18 19:56:36 -0400

    Created all classes basic master loop.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace CLPJ2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        /*Add a check at each prompt if the user inputs "Q" or "q" to quit.*/
+            bool masterLoop = true;
+            while (masterLoop)
+            {
+            /*Create a Template class and call methods here. The Template class will pull from disk.*/
+                Console.WriteLine("Choose a template or type \"Q\" to quit. (Type anything)");
+                String userTemplate = Console.ReadLine();
+                if (userTemplate.ToUpper() == "Q")
+                {
+                    masterLoop = false;
+                    break;
+                }
+            /*Create a SendEmail class and call methods here*/
+                Console.WriteLine("Enter recipients or type \"Q\" to quit. (Type anything)");
+                String userRecipients = Console.ReadLine();
+                if (userRecipients.ToUpper() == "Q")
+                {
+                    masterLoop = false;
+                    break;
+                }
+                Console.WriteLine($"Sending {userTemplate} to {userRecipients}.");
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,17 +1,33 @@
-# 
+# Code Louisville Project 2 (CLPJ2)
+
+
 User selects a template and will be asked questions on values to fill in and the result will be written to disk. The program will then ask if they'd like to do another. 
 
 1. Master loop
     - Ask user after each template if they'd like to do another.
     - After applying 1 template, send to other recipients? 
+        ? Multiple single emails and multiple recipients per each email ?
 2. Loading template from disk.
     - Load templates from file and display name to user.
+        ? User created template list (like contacts) or hard coded ?
 3. Store dictionary
     - Store answers.
+        ? What exactly is the dictionary ?
 4. Unit test 
     - Test templates to match criteria. 
+        ? What exactly is a unit test ? 
 
 
 
 Stretch Goal
 Contact list
+
+    Contact: {Codie Sharp (codie.sharp@gmail.com)} ->
+
+{
+    to: {codie.sharp@gmail.com}
+
+    {Codie},
+    
+    {Template}
+}
